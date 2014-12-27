9916b81d1f505d87f22eb18799654342637a712f
Sylvianguy <sylvia.nguyen12@gmail.com>
2014-12-27 14:06:04 -0500

    implemented new plugin for slider that runs faster

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,25 +1,35 @@
-$(function() {
+// $(function() {
 
-	$('#carousel').carouFredSel({
-		width: '100%',
-		items: {
-			visible: 'odd+2'
-		},
-		auto: {
-			items: 1,
-			easing: 'linear',
-			duration: 9000,
-			timeoutDuration: 0
-		}
+// 	$('#carousel').carouFredSel({
+// 		width: '100%',
+// 		items: {
+// 			visible: 'odd+2'
+// 		},
+// 		auto: {
+// 			items: 1,
+// 			easing: 'linear',
+// 			duration: 9000,
+// 			timeoutDuration: 0
+// 		}
 		
-	});
+// 	});
 
 
+// });
 
-	// $('.caroufredsel_wrapper').css({
-	// 	'width': '100%',
-	// 	'height': '100vh'
-	// });
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+    minSlides: 2,
+    maxSlides: 2,
+    slideWidth: 700,
+    slideMargin: 10,
+    ticker: true,
+    speed: 10000
+  });
 
+  $('.bxslider li').css({
+  	width: 400
+  });
 });
+
 
